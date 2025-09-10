fecd3b568ecb13787e1d947976e4bcb33a687b65
Nayelisjaimes <151255739+Nayelisjaimes@users.noreply.github.com>
2025-09-10 12:06:38 -0500

    division

---
+++ nayelis.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+numero_uno = 1
+numero_dos = 5
+
+suma = numero_uno + numero_dos 
+print("suma = " , suma)
+
+resta = numero_uno - numero_dos 
+print("resta = ", resta)
+
+multiplicacion = numero_uno * numero_dos
+print ("multiplicacion = ", multiplicacion)
+
+division = numero_dos / numero_uno
+print ("division = ", division)
